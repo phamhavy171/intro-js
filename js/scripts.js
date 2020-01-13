d28ee50554c2e7c84c9d7efcca818d82434d2ca4
phamhavy171 <59789678+phamhavy171@users.noreply.github.com>
2020-01-13 13:19:54 +0200

    add some javascript

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+console.log('that was it');
+
+
+var a = 2;
+var b = 2;
+var c = a + b;
+
+if (a < b) { 
+    /* do if a is 
+    lesser than b*/
+    console.log('a is lessr than b');
+    console.log(a + ' is lesser than ' + b);
+}
+else { 
+    // do otherwise
+    console.log(' a is not greater than b');
+}
+
+/* this is for the laziness */
+
+if ( a < b) {
+    console.log('a is lesser than b');
+} 
+if ( a > b) {
+    console.log('a is geater than b');
+}
+if (a === 1) {
+    console.log('a is exactly 1');
+}
