a5dc7bd77b7c341becc3ffb1cb7bac66fc653a8a
phamhavy171 <59789678+phamhavy171@users.noreply.github.com>
2020-01-27 14:30:15 +0200

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,49 @@
-(function () {
-    var itemClassName = 'slider__photo';
-        items = document.getElementsByClassName(itemClassName),
-        totalItems = items.length,
-        slide = 0;
+(function() {
+  var itemClassName = "slider__photo";
+  (items = document.getElementsByClassName(itemClassName)),
+    (totalItems = items.length),
+    (slide = 0);
 
-    // Set event listeners
-    function setEventListeners() {
-        var next = document.getElementsByClassName('slider__button--next')[0],
-            prev = document.getElementsByClassName('slider__button--prev')[0];
+  // Set event listeners
+  function setEventListeners() {
+    var next = document.getElementsByClassName("slider__button--next")[0],
+      prev = document.getElementsByClassName("slider__button--prev")[0];
 
-        next.addEventListener('click', getNext);
-        prev.addEventListener('click', getPrev);
+    next.addEventListener("click", getNext);
+    prev.addEventListener("click", getPrev);
+  }
+
+  function getNext() {
+    // TODO
+    items[slide].classList.remove("active");
+    if (slide === totalItems - 1) {
+      slide = 0;
+    } else {
+      slide++;
     }
+    items[slide].classList.add("active");
+  }
 
-    function getNext() {
-        // TODO
+  // slide = (slide === totalItems - 1) ? 0 : slide++;
+
+  function getPrev() {
+    // TODO
+    items[slide].classList.remove("active");
+    if (slide === 0) {
+      slide = totalItems - 1;
+    } else {
+      slide--;
     }
+    items[slide].classList.add("active");
+  }
 
-    function getPrev(){
-        // TODO      
-    }
+  function initSlider() {
+    // setInitialClasses();
+    setEventListeners();
 
-    function initSlider() {
-        // setInitialClasses();
-        setEventListeners();
-        
-        // Set moving to false so that the slider becomes interactive
-        moving = false;
-    }
+    // Set moving to false so that the slider becomes interactive
+    moving = false;
+  }
 
-    initSlider();
+  initSlider();
 })();
