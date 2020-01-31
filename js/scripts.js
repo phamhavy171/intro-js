32a801abf57bb16a3c43abe7ab3872abc5461030
phamhavy171 <59789678+phamhavy171@users.noreply.github.com>
2020-01-31 14:40:06 +0200

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,40 @@
-console.log('that was it');
+jQuery(document).ready(function() {
+  var btn = $(".totop");
 
-}
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass("totop");
+    } else {
+      btn.removeClass("totop");
+    }
+  });
 
+  btn.on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
+});
+
+$(document).ready(function() {
+  function blinklogo() {
+    $("#logo").click();
+  }
+
+  $("#logo").click(function() {
+    $("#logo")
+      .fadeTo(100, 0.5)
+      .fadeTo(100, 1, blinklogo);
+  });
+});
+
+$(document).ready(function() {
+  $('[data-type="proteinbar"]').hover(function() {
+    $('[data-type="proteinbar"]').css("background", "blue");
+  });
+});
+
+$(document).ready(function() {
+  $('[data-type="mineralwater"]').hover(function() {
+    $('[data-type="mineralwater"]').css("background", "green");
+  });
+});
